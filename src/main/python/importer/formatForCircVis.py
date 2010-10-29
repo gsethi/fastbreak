8b261268aa6a72f5a3ff18804378e1e0e76d9500
rbressle <rbressle@localhost>
2010-10-29 22:41:08 +0000

    scripts to format for various

---
+++ src/main/python/importer/formatForCircVis.py
@@ -0,0 +1,34 @@
+__author__ = 'rbressle'
+import genelist.py
+
+
+def formatMatrix(matrixfilen, gl):
+    fo = open(mstrixfilen,"r")
+    fout = open(mstrixfilen+".for.circvis.txt","w")
+    fout.write("chr1\tstart1\tend1\toptions1\tchr2\tstart2\tend2\toptions2\tlinkValue\n")
+    columns = fo.next().lstrip().rstrip().replace('"','').split("\t")
+    for line in fo:
+    	vals = line.rstrip().replace('"','').split("\t")
+    	gene = vals[0]
+    	for i,val in enumerate(vals[1:])
+    		if columns[i]!=gene and float(val)<1.0:
+    		go1 = gl[gene]
+    		go2 = gl[columns[i]]
+    		fout.write("\t".join([go1["chr"],go1["start"],go1["end"],"label="+go1["name"],go2["chr"],go2["start"],go2["end"],"label="+go2["name"],val])+"\n")
+    fout.close()
+    fo.close()
+    			
+
+def formatList(listfilen,gl):
+    fo = open(listfilen,"r")
+    fout = open(mstrixfilen+".for.circvis.txt","w")
+
+
+
+if __name__ == "__main__":
+
+    gl = genelist.loadGenesByName(sys.argv[1])
+    
+    
+
+  
--- src/main/python/importer/genelist.py
+++ src/main/python/importer/genelist.py
@@ -11,3 +11,14 @@ def loadGenes(filen,types):
 		genes[vs[2]].append(vdict)
 	genelist.close()
 	return genes
+
+def loadGenesByName(filen):
+	genes={}
+	genelist = open (filen,"r")
+	for gene in genelist:
+		vs = gene.strip().split("\t")
+		if not vs[0] in genes:
+		    vdict = {"name":vs[0],"chr":vs[2], "start":int(vs[4]),"end":int(vs[5]),"or":vs[3]}
+		    genes[vs[0]].append(vdict)
+	genelist.close()
+	return genes
