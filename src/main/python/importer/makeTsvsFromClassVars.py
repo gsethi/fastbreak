4fd62da23d910502626a374747217e3ee2d251f7
rbressle <rbressle@localhost>
2010-11-30 06:34:30 +0000

    make tsvs for submission

---
+++ src/main/python/importer/makeTsvsFromClassVars.py
@@ -0,0 +1,27 @@
+foin = open("ClassVarsOld.txt","r")
+foin.next()
+
+patients = {}
+for line in foin:
+	line = line.rstrip().replace(".","-").split("\t")
+	patient = line[0][0:12]
+	print patient
+	
+	patients[patient]={"line":"\t".join([patient,line[2],"Status: "+line[3]+" Resistance: "+line[4]])+"\n"}
+	patients[patient][line[1]] = line
+	
+samplefo = open("samples.tsv","w")
+patientfo = open("patients.tsv","w")
+
+for patient in patients.keys():
+	if "CANCER" in patients[patient] and "BLOOD" in patients[patient]:
+		patientfo.write(patients[patient]["line"])
+		
+		ts = patients[patient]["CANCER"]
+		bl = patients[patient]["BLOOD"]
+		samplefo.write("\t".join([ts[0],patient,"Tumor",bl[0]," "," "]))
+		samplefo.write("\t".join([bl[0],patient,"Blood",ts[0]," "," "]))
+		
+samplefo.close()
+patientfo.close()
+	
