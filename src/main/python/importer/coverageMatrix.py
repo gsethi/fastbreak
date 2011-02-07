7f3a1976590b6353ab11a1c2ef2dd18980e348f0
rbressle <rbressle@localhost>
2011-02-07 21:04:25 +0000

    added cutoff to coverageCode

--- src/main/python/importer/coverageMatrix.py
+++ src/main/python/importer/coverageMatrix.py
@@ -3,6 +3,8 @@ import makeCalls, genelist
 resolution = 1000
 padby = 1000
 includeAllGenes = True
+restricttoCoveredBins = True
+coveragecutoff = 1
 
 def score(genelistfn):
 	wigfiles = glob.glob("./*.tile.wig")
@@ -12,6 +14,34 @@ def score(genelistfn):
 	print "Loading " + genelistfn
 	genes=genelist.loadGenes(genelistfn,wigfiles)
 	
+	coveredregions  = {}
+	firstpass = True
+	
+	if restricttoCoveredBins == True:
+		print "Finding Covered Regions"
+		for wigfn in wigfiles:		
+			print "Loading " + wigfn
+			wig =  makeCalls.loadWigHash(wigfn)
+			fitleredcount = 0
+			passedCount = 0
+			for chr in wig:
+				chrNorm = addChr(chr)
+				if not chrNorm in coveredregions:
+					coveredregions[chrNorm] = {}
+				for tile in wig[chr]:
+					if int(wig[chr][tile]) >= int(coveragecutoff):
+						passedCount +=1
+						if firstpass == True:
+							coveredregions[chrNorm][tile] = True
+										
+					else:
+						coveredregions[chrNorm][tile] = False
+						fitleredcount += 1
+			print "Found %i tiles covered over cutoff and %i tiles covered but below cutoff"%(passedCount,fitleredcount)
+						
+			firstpass = False
+	
+	
 	for wigfn in wigfiles:		
 		print "Loading " + wigfn
 		wig =  makeCalls.loadWigHash(wigfn)
@@ -20,18 +50,20 @@ def score(genelistfn):
 		for chr in genes:
 			for j, gene in enumerate(genes[chr]):
 				for tile in xrange(makeCalls.getTile(int(gene["start"])-int(padby)),makeCalls.getTile(int(gene["end"])+int(padby))):
-					wigchr=chr
-					if not wigchr in wig:
-						wigchr = wigchr.replace("chr","")
-					if wigchr in wig and tile in wig[wigchr]:
-						if includeAllGenes == False:
-							if int(wig[wigchr][tile])>10:
-								genes[chr][j][wigfn] = 1
-						else:
-							if int(wig[wigchr][tile])>0:
-								if not wigfn in genes[chr][j]:
-									genes[chr][j][wigfn] = 0
-								genes[chr][j][wigfn]+=int(wig[wigchr][tile])
+					if restricttoCoveredBins == False or ( chr in coveredregions and tile in coveredregions[chr] and coveredregions[chr][tile] == True):
+						wigchr=chr
+						
+						if not wigchr in wig:
+							wigchr = wigchr.replace("chr","")
+						if wigchr in wig and tile in wig[wigchr]:
+							if includeAllGenes == False:
+								if int(wig[wigchr][tile])>10:
+									genes[chr][j][wigfn] = 1
+							else:
+								if int(wig[wigchr][tile])>0:
+									if not wigfn in genes[chr][j]:
+										genes[chr][j][wigfn] = 0
+									genes[chr][j][wigfn]+=int(wig[wigchr][tile])
 		
 								
 	
