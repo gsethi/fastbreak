1defe74d64e609028cd263cd6d7a565dfdc5a187
rbressle <rbressle@localhost>
2011-03-18 21:09:52 +0000

    moved old importer dir

--- src/main/python/fastbreak/fullPass2.py
+++ src/main/python/fastbreak/fullPass2.py
@@ -1,3 +1,29 @@
+#!/usr/bin/python
+#
+# 
+#     Copyright (C) 2003-2010 Institute for Systems Biology
+#                             Seattle, Washington, USA.
+# 
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License as published by the Free Software Foundation; either
+#     version 2.1 of the License, or (at your option) any later version.
+# 
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+# 
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+# 
+
+"""
+"""
+
+__author__ = "Ryan Bressler"
+
 #example usage (in the directory with jakes output)
 #python fullPass2.py TCGA-25-1328 TCGA-25-1319 TCGA-25-1314
 
@@ -5,8 +31,8 @@ import sys
 import os
 import glob
 
-from fastbreak import plotL
-from fastbreak import makeCalls
+import plotL
+import makeCalls
 
 do1stplots = False
 makecalls = True
