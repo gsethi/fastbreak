0b213239bb2a18d5012cd7522ba6fb7a1b7cbbf1
rbressle <rbressle@localhost>
2011-03-18 21:35:51 +0000

    cleanup

--- src/main/python/transplantdata.py
+++ src/main/python/transplantdata.py
@@ -1,12 +1,41 @@
 #!/usr/bin/python
-#^ this must point to python>= 2.5 on your system !!!
-#| 
-# This is the data service for use with the transplant visaulization
-# there is a cgi python version floating arount to but
-# this version you call by passing in the query string
-# python transplantdata.py querystring
-# see below for the expected params
-import os, glob
+#
+# 
+#     Copyright (C) 2003-2010 Institute for Systems Biology
+#                             Seattle, Washington, USA.
+# 
+#     This library is free software; you can redistribute it and/or
+#     modify it under the terms of the GNU Lesser General Public
+#     License as published by the Free Software Foundation; either
+#     version 2.1 of the License, or (at your option) any later version.
+# 
+#     This library is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#     Lesser General Public License for more details.
+# 
+#     You should have received a copy of the GNU Lesser General Public
+#     License along with this library; if not, write to the Free Software
+#     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA
+# 
+
+"""
+This is the data service for use with the transplant visaulization
+there is a cgi python version floating arount to but
+this version you call by passing in the query string
+python transplantdata.py querystring
+see below for the expected params.
+
+Python >= 2.5 required
+"""
+
+__author__ = "Ryan Bressler"
+
+
+
+
+import os
+import glob
 try: import json #python 2.6 included simplejson as json
 except ImportError: import simplejson as json
 try: import cPickle as pickle
