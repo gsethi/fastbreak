91c1fd05836188a9b719cad2d47d8ba867ad3157
rbressle <rbressle@localhost>
2011-03-18 23:47:16 +0000

    minor pychecker fixes

--- src/main/python/fastbreak/tsvparser.py
+++ src/main/python/fastbreak/tsvparser.py
@@ -27,13 +27,7 @@ __author__ = "Ryan Bressler"
 def splitLine(line):
 	return line.rstrip("\n").split("\t")
 	
-def parseLine(line,headerarg=False):
-	global headers
-	h = []
-	if not headerarg:
-		h = headers
-	else:
-		h	= headerarg
+def parseLine(line,headerarg):
 	rv = {}
 	for i,v in enumerate(splitLine(line)):
 		rv[h[i]]=v
